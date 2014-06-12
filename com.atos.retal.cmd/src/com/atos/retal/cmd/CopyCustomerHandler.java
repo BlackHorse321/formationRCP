9db9865a70bd553c62950abae5910cd64f78fe80
formation <formation@192.168.130.106>
2014-06-12 23:36:07 +0200

    commande avec filtre

---
+++ com.atos.retal.cmd/src/com/atos/retal/cmd/CopyCustomerHandler.java
@@ -0,0 +1,46 @@
+package com.atos.retal.cmd;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.RTFTransfer;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
+
+public class CopyCustomerHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+
+		if (selection instanceof IStructuredSelection) {
+			Object obj = ((IStructuredSelection) selection).getFirstElement();
+
+			if (obj instanceof Customer) {
+				Clipboard clipboard = new Clipboard(Display.getCurrent());
+				String textData = ((Customer) obj).getDisplayName();
+
+				String rtfData = "{\\rtf1\\b\\i " + textData + "}";
+
+				TextTransfer textTransfer = TextTransfer.getInstance();
+				RTFTransfer rtfTransfer = RTFTransfer.getInstance();
+				Transfer[] transfers = new Transfer[] { textTransfer,
+						rtfTransfer };
+				Object[] data = new Object[] { textData, rtfData };
+				clipboard.setContents(data, transfers);
+				clipboard.dispose();
+			}
+		}
+		return null;
+	}
+
+}
